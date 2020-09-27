ddaca684e6b01ab4d6d88717c8756002110c9a60
Y1a2s3h4 <yashpurohit763@gmail.com>
2020-09-27 17:51:24 +0530

    removed port error for heroku

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-const port = 3000 || process.env.PORT;
 const { carNumber } = require("./Route/Number.js");
 app.get("/:term", async (req, res) => {
   const term = req.params.term;
   console.log(term);
   res.send(await carNumber(term));
 });
-app.listen(port, () => {
-  console.log(`running on http://localhost:${port}`);
-});
+app.listen(process.env.PORT || 3000);
