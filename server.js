88162a41b026e38f5b6b52b264a62a5f0728c44f
Y1a2s3h4 <yashpurohit763@gmail.com>
2020-09-28 17:56:22 +0530

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const app = express();
+const express = require("express"),
+  app = express(),
+  cors = require("cors");
 const { carNumber } = require("./Route/Number.js");
+
+app.use(cors());
 app.get("/:term", async (req, res) => {
   const term = req.params.term;
   console.log(term);
